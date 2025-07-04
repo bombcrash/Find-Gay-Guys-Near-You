fbfb734e490bfa5426105f7417119ae8d6f1a5af
bombcrash <156601330+bombcrash@users.noreply.github.com>
2025-07-04 20:40:01 +0300

    Update FindGayGuysNearYou.py

--- FindGayGuysNearYou.py
+++ FindGayGuysNearYou.py
@@ -1,15 +1,22 @@
 import random
 import os
+import sys
 
-# Windows konsolda başlığı ayarla
+# Başlığı değiştir
 os.system("title Find Gay Guys Near You")
 
-# ANSI Escape kodlarıyla mor renk
+# Konsolu temizle
+os.system("cls")
+
+# Mor renk ANSI kodu
 PURPLE = "\033[95m"
 RESET = "\033[0m"
 
 # Rastgele sayı üret
 number = random.randint(0, 1000)
 
-# Çıktıyı mor renkte yaz
+# Çıktıyı mor renkte yazdır
 print(f"{PURPLE}Number of Gay Guys Near You = {number}{RESET}")
+
+# Çıkmadan beklet
+input("\nPress Enter to exit...")
